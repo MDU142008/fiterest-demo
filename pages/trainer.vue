d7e5e2b60fa6596af9de1a77e0e70427ba192e18
MDU142008 <manueldiazusandi2008@gmail.com>
2025-06-11 19:07:44 -0300

    Add training and trainer pages

---
+++ pages/trainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center text-center">
+    <h1 class="text-3xl font-bold">Welcome, Personal Trainer!</h1>
+  </div>
+</template>
---
+++ pages/training.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center text-center">
+    <h1 class="text-3xl font-bold">Welcome to the Training Plan</h1>
+  </div>
+</template>
